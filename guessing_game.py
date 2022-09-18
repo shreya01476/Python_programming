79ed43c39ed1112d9a785a50fbc496f30087336e
shreya01476 <shreya.gupta_cs19@gla.ac.in>
2022-09-18 16:06:40 +0530

    Here I written a code of guessing game in python

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,18 @@
+import math
+import random
+secret_number=22
+guess_count = 0
+guess_limit = 3
+while guess_count<=guess_limit:
+    guessing_number=int(input("Enter the number:"))
+    guess_count+=1
+    if guessing_number==secret_number:
+        print("Yaay!You Won")
+        break
+    elif guessing_number < secret_number:
+        print("A Big No!!!")
+    elif guessing_number > secret_number:
+        print("Tooooo Bigg!!")
+    else:
+        print("Better luck next time")
 
